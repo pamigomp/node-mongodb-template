dd1780dd8a254355fe6d1d9f0bba55e9c78899da
Michal Pietrzak <201011@student.pwr.wroc.pl>
2019-07-26 21:52:43 +0200

    Add default image source

--- app/models/schemas/image.js
+++ app/models/schemas/image.js
@@ -5,7 +5,7 @@ const timestampPlugin = require('../plugins/timestamp');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    url: {type: String, unique: true, default: '<default_product_image>', maxlength: 128},
+    url: {type: String, unique: true, default: 'https://bookyourstock.com/index.php/home/vendor_profile/get_slider/', maxlength: 128},
     productId: {type: mongoose.Types.ObjectId, required: true, unique: true}
 });
 
