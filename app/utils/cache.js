5a373834bfc3d57b934ffa38db98ec3ac1af2e0c
Michal Pietrzak <201011@student.pwr.wroc.pl>
2019-07-26 22:59:34 +0200

    Add possibility to override default cache duration

--- app/utils/cache.js
+++ app/utils/cache.js
@@ -5,27 +5,25 @@ if (process.env.NODE_ENV === 'development') {
     mcache.debug(true);
 }
 
-const HOUR_IN_SECONDS = 60 * 60;
+const HOUR_IN_SECONDS = process.env.DEFAULT_CACHE_DURATION || 60 * 60;
 
-module.exports = (duration = HOUR_IN_SECONDS) => {
-    return (req, res, next) => {
-        let key = '__express__' + req.originalUrl || req.url;
-        let cachedBody = mcache.get(key);
-        if (cachedBody) {
-            try {
-                cachedBody = JSON.parse(cachedBody);
-            } finally {
-                res.send(cachedBody);
-            }
-        } else {
-            res.sendResponse = res.send;
-            res.send = (body) => {
-                if (res.statusCode.toString().startsWith('2')) {
-                    mcache.put(key, body, duration * 1000);
-                }
-                res.sendResponse(body);
-            };
-            next();
+module.exports = (duration = HOUR_IN_SECONDS) => (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url;
+    let cachedBody = mcache.get(key);
+    if (cachedBody) {
+        try {
+            cachedBody = JSON.parse(cachedBody);
+        } finally {
+            res.send(cachedBody);
         }
-    };
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            if (res.statusCode.toString().startsWith('2')) {
+                mcache.put(key, body, duration * 1000);
+            }
+            res.sendResponse(body);
+        };
+        next();
+    }
 };
